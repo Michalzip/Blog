7605247b081cfa3a7063290888ecbf829bea88b5
Michał Potoczny <potoczny011@gmail.com>
2024-09-18 20:25:06 +0200

    delete ds store file

--- blog-frontend/src/components/OffCanvasMenu/OffCanvasMenu.jsx
+++ blog-frontend/src/components/OffCanvasMenu/OffCanvasMenu.jsx
@@ -1,5 +1,6 @@
 import classess from "./OffCanvasMenu.module.css";
 //to dac do PostDetailPage
+//https://codepen.io/maclevison/pen/OJJgbW
 function OffCanvasMenu() {
   return <div className={classess.sidenav}></div>;
 }
